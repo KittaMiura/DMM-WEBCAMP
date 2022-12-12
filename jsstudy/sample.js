b8e9a710e43b57a06c4a212afcbe2566d865630e
Kitta Miura <roppou8818@gmail.com>
2022-12-12 17:32:46 +0000

    [Add]skipper_app

--- jsstudy/sample.js
+++ jsstudy/sample.js
@@ -1,25 +1,41 @@
+// じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-
-let js_hand = getJShand();
-
-let judge = winLose(user_hand, js_hand);
-
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
-function getJShand(){
-  let js_hand_num = Math.floor( Math.random() * 3 );
-
-  if(js_hand_num == 0){
-    js_hand = "グー";
-  } else if(js_hand_num == 1){
-    js_hand = "チョキ";
-  } else if(js_hand_num == 2){
-    js_hand = "パー";
-  }
-
-  return js_hand;
+// グーチョキパー以外が選択されたときの対応
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 }
 
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+if (user_hand != null) {
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
 function winLose(user, js){
   let winLoseStr;
 
